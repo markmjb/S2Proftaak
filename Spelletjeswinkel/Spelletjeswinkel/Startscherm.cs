7ba4d50e0ef89297feb05f8432318f29ebddf670
markmjb <m.blesgraaf@student.fontys.nl>
2015-03-07 13:01:20 +0100

    First commit

---
+++ Spelletjeswinkel/Spelletjeswinkel/Startscherm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spelletjeswinkel
+{
+    public partial class Startscherm : Form
+    {
+        public Startscherm()
+        {
+            InitializeComponent();
+        }
+    }
+}
