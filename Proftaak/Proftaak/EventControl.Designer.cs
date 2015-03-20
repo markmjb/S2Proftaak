1b6dc69523eaeb9f29764442c54e5b40597a6628
markmjb <m.blesgraaf@student.fontys.nl>
2015-03-20 16:01:22 +0100

    Event Control Added

--- Proftaak/Proftaak/EventControl.Designer.cs
+++ Proftaak/Proftaak/EventControl.Designer.cs
@@ -28,20 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DTPstart = new System.Windows.Forms.DateTimePicker();
+            this.DTPstop = new System.Windows.Forms.DateTimePicker();
+            this.tbEventname = new System.Windows.Forms.TextBox();
+            this.tbPriceperticket = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Event";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(531, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Start Date: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "End Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Price Per Ticket: ";
+            // 
+            // DTPstart
+            // 
+            this.DTPstart.Location = new System.Drawing.Point(75, 251);
+            this.DTPstart.Name = "DTPstart";
+            this.DTPstart.Size = new System.Drawing.Size(220, 20);
+            this.DTPstart.TabIndex = 7;
+            // 
+            // DTPstop
+            // 
+            this.DTPstop.Location = new System.Drawing.Point(75, 302);
+            this.DTPstop.Name = "DTPstop";
+            this.DTPstop.Size = new System.Drawing.Size(220, 20);
+            this.DTPstop.TabIndex = 8;
+            // 
+            // tbEventname
+            // 
+            this.tbEventname.Location = new System.Drawing.Point(75, 206);
+            this.tbEventname.Name = "tbEventname";
+            this.tbEventname.Size = new System.Drawing.Size(220, 20);
+            this.tbEventname.TabIndex = 9;
+            // 
+            // tbPriceperticket
+            // 
+            this.tbPriceperticket.Location = new System.Drawing.Point(411, 206);
+            this.tbPriceperticket.Name = "tbPriceperticket";
+            this.tbPriceperticket.Size = new System.Drawing.Size(195, 20);
+            this.tbPriceperticket.TabIndex = 10;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(316, 238);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(66, 13);
+            this.lblDescription.TabIndex = 11;
+            this.lblDescription.Text = "Description: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(319, 257);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 65);
+            this.textBox1.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(598, 150);
+            this.dataGridView1.TabIndex = 13;
             // 
             // EventControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 362);
+            this.ClientSize = new System.Drawing.Size(618, 362);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.tbPriceperticket);
+            this.Controls.Add(this.tbEventname);
+            this.Controls.Add(this.DTPstop);
+            this.Controls.Add(this.DTPstart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "EventControl";
             this.Text = "EventControl";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EventControl_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker DTPstart;
+        private System.Windows.Forms.DateTimePicker DTPstop;
+        private System.Windows.Forms.TextBox tbEventname;
+        private System.Windows.Forms.TextBox tbPriceperticket;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
