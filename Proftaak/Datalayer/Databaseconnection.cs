add6636ce6fbfe19b1ac26a9e97d31cd86a17785
markmjb <m.blesgraaf@student.fontys.nl>
2015-03-20 13:22:55 +0100

    Just because I can

--- Proftaak/Datalayer/Databaseconnection.cs
+++ Proftaak/Datalayer/Databaseconnection.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using Oracle.DataAccess;
 
 namespace Datalayer
 {
     public class Databaseconnection
     {
-
+    //Open connection, close connection , new connection, overall Methods.
 
     }
 }
