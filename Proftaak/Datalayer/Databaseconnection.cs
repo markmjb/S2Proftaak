a90816c287d23eb3dcb7fafb91eda57ecce70e14
markmjb <m.blesgraaf@student.fontys.nl>
2015-04-10 14:44:01 +0200

    Snippet OracleReader;
    ,

--- Proftaak/Datalayer/Databaseconnection.cs
+++ Proftaak/Datalayer/Databaseconnection.cs
@@ -36,6 +36,38 @@ namespace Datalayer
         {
             this.conn.Close();
         }
+
+        //public List<DatalayerComment> CommentsOfFile(string item)
+        //{
+        //    string selectString = "SELECT * FROM PTS_COMMENTFILE WHERE SUBJECT Like '" + item + "%'";
+        //    this.command = new OracleCommand(selectString, this.conn);
+        //    DCommentList = new List<DatalayerComment>();
+        //    try
+        //    {
+        //        this.conn.Open();
+        //        OracleDataReader reader = this.command.ExecuteReader();
+
+        //        while (reader.Read())
+        //        {
+        //            int Commentid = Convert.ToInt32(reader["COMMENTID"]);
+        //            string Subject = Convert.ToString(reader["SUBJECT"]);
+        //            string Username = Convert.ToString(reader["USERNAME"]);
+        //            string Description = Convert.ToString(reader["DESCRIPTION"]);
+        //            DCommentList.Add(new Datalayer.DatalayerComment(Commentid, Subject, Username, Description));
+        //        }
+        //    }
+        //    catch (OracleException exc)
+        //    {
+        //        Console.WriteLine(exc);
+        //    }
+        //    finally
+        //    {
+        //        conn.Close();
+        //    }
+
+        //    return DCommentList;
+        //}
+
     }
 }
 
