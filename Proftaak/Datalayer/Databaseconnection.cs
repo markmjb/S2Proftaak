01e44c021b68ced6d0806610495350694606dd70
markmjb <m.blesgraaf@student.fontys.nl>
2015-04-10 09:30:02 +0200

    Databaseconnection template
    Databaseconnection template

--- Proftaak/Datalayer/Databaseconnection.cs
+++ Proftaak/Datalayer/Databaseconnection.cs
@@ -5,12 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms.VisualStyles;
 using Oracle.DataAccess;
+using Oracle.DataAccess.Client;
 
 namespace Datalayer
 {
     public class Databaseconnection
     {
-    //Open connection, close connection , new connection, overall Methods.
+        private OracleConnection conn;
 
+        public Databaseconnection()
+        {
+            //Oude proftaak connection
+  
+                conn = new OracleConnection();
+                string user = "system";
+                string pw = "system";
+                conn.ConnectionString = "User Id=" + user + ";Password=" + pw + ";Data Source=" + "127.0.0.1/" + ";";
+        }
+  
     }
 }
+
