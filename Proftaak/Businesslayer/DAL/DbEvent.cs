2a2d000b17863edb808d490e00d980bc3bdc3565
salami350 <salami350@live.nl>
2015-04-19 17:02:08 +0200

    EventControl is done!
    You can edit, Create, and Delete events

--- Proftaak/Businesslayer/DAL/DbEvent.cs
+++ Proftaak/Businesslayer/DAL/DbEvent.cs
@@ -20,7 +20,7 @@ namespace Businesslayer.DAL
             
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 cmd.CommandText = "SELECT E.eventID, E.eventName, E.description, E.startDate, E.endDate, E.ticketPrice, E.userID, A.addressID, A.country, A.province, A.city, A.street, A.housenumber, A.postalcode FROM PTS2_EVENT E, PTS2_ADDRESS A WHERE E.addressID = A.addressID";
                 
                 db.Connection.Open();
@@ -65,50 +65,13 @@ namespace Businesslayer.DAL
             return events;
         }
 
-        public bool CheckEvent(string name, string description, DateTime startDate, DateTime endDate, decimal ticketPrice)
-        {            
-            bool result = false;
-
-            try
-            {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
-                
-                cmd.CommandText = "SELECT COUNT(eventID) as count FROM PTS2_EVENT WHERE eventName = :name AND description = :description AND startDate = TO_DATE(:startDate, 'MM/DD/YYYY') AND endDate = TO_DATE(:endDate, 'MM/DD/YYYY') AND ticketPrice = :ticketPrice";
-                cmd.Parameters.Add("name", name);
-                cmd.Parameters.Add("description", description);
-                cmd.Parameters.Add("startDate", startDate.ToShortDateString());
-                cmd.Parameters.Add("endDate", endDate.ToShortDateString());
-                cmd.Parameters.Add("ticketPrice", ticketPrice);
-
-                db.Connection.Open();
-
-                OracleDataReader reader = cmd.ExecuteReader();
-
-                reader.Read();
-                if (Convert.ToInt32(reader["count"]) > 0)
-                {
-                    result = true;
-                }  
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            finally
-            {
-               db.Connection.Close();
-            }
-
-            return result;  
-        }
-
         public bool CheckAddress(string country, string province, string city, string street, int streetnumber, string postalcode)
         {
             bool result = false;
 
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 
                 cmd.CommandText = "SELECT COUNT(addressID) as count FROM PTS2_ADDRESS WHERE country = :country AND province = :province AND city = :city AND street = :street AND houseNumber = :streetnumber AND postalcode = :postalcode";
                 cmd.Parameters.Add("country", country);
@@ -143,7 +106,7 @@ namespace Businesslayer.DAL
         {
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 
                 cmd.CommandText = "INSERT INTO PTS2_ADDRESS (country, province, city, street, housenumber, postalcode) VALUES (:country, :province, :city, :street, :housenumber, :postalcode)";
                 cmd.Parameters.Add("country", country);
@@ -170,7 +133,7 @@ namespace Businesslayer.DAL
         {
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 
                 cmd.CommandText = "INSERT INTO PTS2_Event (eventName, description, startDate, endDate, ticketPrice, userID, addressID) VALUES (:name, :description, TO_DATE(:startDate, 'MM/DD/YYYY'), TO_DATE(:endDate, 'MM/DD/YYYY'), :ticketPrice, :userID, :addressID)";
                 cmd.Parameters.Add("name", name);
@@ -207,7 +170,7 @@ namespace Businesslayer.DAL
 
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 
                 cmd.CommandText = "SELECT addressID FROM PTS2_ADDRESS WHERE country = :country AND province = :province AND city = :city AND street = :street AND housenumber = :streetnumber AND postalcode = :postalcode";
                 cmd.Parameters.Add("country", country);
@@ -244,49 +207,11 @@ namespace Businesslayer.DAL
             return addressID;
         }
 
-        public int GetEventID(string eventName, string description, DateTime startDate, DateTime endDate, decimal ticketPrice)
-        {
-            int eventID = -1;
-
-            try
-            {
-                OracleCommand cmd = db.Connection.CreateCommand();
-                
-                cmd.CommandText = "SELECT eventID FROM PTS2_EVENT WHERE eventName = :eventName AND description = :description AND startDate = TO_DATE(:startDate, 'MM/DD/YYYY') AND endDate = TO_DATE(:endDate, 'MM/DD/YYYY') AND ticketPrice = :ticketPrice";
-                cmd.Parameters.Add("eventName", eventName);
-                cmd.Parameters.Add("description", description);
-                cmd.Parameters.Add("startDate", startDate.ToShortDateString());
-                cmd.Parameters.Add("endDate", endDate.ToShortDateString());
-                cmd.Parameters.Add("ticketPrice", ticketPrice);
-
-                db.Connection.Open();
-                OracleDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    if (reader.IsDBNull(reader.GetOrdinal("eventID")))
-                    {
-                        eventID = Convert.ToInt32(reader["eventID"]);                                                                    
-                    }
-            
-                }
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            finally
-            {
-               db.Connection.Close();
-            }
-            return eventID;
-        }
-
         public void DeleteEvent(int eventID)
         {
             try
             {
-                OracleCommand cmd = this.db.Connection.CreateCommand();
+                OracleCommand cmd = db.Connection.CreateCommand();
                 
                 cmd.CommandText = "DELETE FROM PTS2_EVENT WHERE eventID = :eventID";
                 cmd.Parameters.Add("eventID", eventID);
@@ -297,6 +222,77 @@ namespace Businesslayer.DAL
             }
             catch (Exception e)
             {
+                throw e;
+            }
+            finally
+            {
+                db.Connection.Close();
+            }
+        }
+
+        public void EditEvent(int eventID, string columnName, string columnValue)
+        {
+            try
+            {
+                OracleCommand cmd = db.Connection.CreateCommand();
+
+                if (columnName == "eventName")
+                {
+                    cmd.CommandText = "UPDATE PTS2_EVENT SET eventName = :columnValue WHERE eventID = :eventID";
+                    cmd.Parameters.Add("columnValue", columnValue);
+                    cmd.Parameters.Add("eventID", eventID);
+                }
+                else if (columnName == "startDate")
+                {
+                    cmd.CommandText =
+                        "UPDATE PTS2_EVENT SET startDate = TO_DATE(:columnValue, 'MM/DD/YYYY') WHERE eventID = :eventID";
+                    cmd.Parameters.Add("columnValue", columnValue);
+                    cmd.Parameters.Add("eventID", eventID);
+                }
+                else if (columnName == "endDate")
+                {
+                    cmd.CommandText =
+                        "UPDATE PTS2_EVENT SET endDate = TO_DATE(:columnValue, 'MM/DD/YYYY') WHERE eventID = :eventID";
+                    cmd.Parameters.Add("columnValue", columnValue);
+                    cmd.Parameters.Add("eventID", eventID);
+                }
+                else if (columnName == "description")
+                {
+                    cmd.CommandText = "UPDATE PTS2_EVENT SET description = :columnValue WHERE eventID = :eventID";
+                    cmd.Parameters.Add("columnValue", columnValue);
+                    cmd.Parameters.Add("eventID", eventID);
+                }
+
+                db.Connection.Open();
+                cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                db.Connection.Close();
+            }
+        }
+
+        public void EditEvent(int eventID, string columnName, decimal columnValue)
+        {
+            try
+            {
+                OracleCommand cmd = db.Connection.CreateCommand();
+
+                cmd.CommandText = "UPDATE PTS2_EVENT SET ticketPrice = :columnValue WHERE eventID = :eventID";
+                cmd.Parameters.Add("columnValue", columnValue);
+                cmd.Parameters.Add("eventID", eventID);
+
+                db.Connection.Open();
+                cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+
                 throw e;
             }
             finally
